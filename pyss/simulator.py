28369f438bc08392bb5eb3b61070e8181fde6d89
Alexandre Decan <alexandre.decan@lexpage.net>
2015-11-30 20:59:18 +0100

    Interface for simulator

--- pyss/simulator.py
+++ pyss/simulator.py
@@ -2,17 +2,49 @@ from pyss import statemachine
 from pyss.evaluator import Evaluator
 
 
-# TODO: Provide an interface, rely on a CodeEvaluator
 class Simulator:
+    def __init__(self, sm: statemachine.StateMachine, evaluator: Evaluator=None):
+        """
+        A simulator that interprets a state machine according to a specific semantic.
+        :param sm: state machine to interpret
+        :param evaluator: Code evaluator (optional)
+        """
+        self.evaluator = evaluator
+        self.sm = sm
+        self.configuration = []  # List of active states
+        self.events = []
+
+    def fire_event(self, event: statemachine.Event):
+        self.events.append(event)
+
+    def microstep(self):
+        """
+        Perform an atomic micro step.
+        """
+        raise NotImplementedError()
+
+    def macrostep(self):
+        """
+        Perform a macro step, ie. a sequence of micro steps until a stable configuration is reached.
+        Corresponds to the processing of exactly ONE transition.
+        """
+        raise NotImplementedError()
+
+    def __repr__(self):
+        return '{}[{}]'.format(self.__class__.__name__, ' '.join(self.configuration))
+
+
+class HierarchicalAutomataSimulator(Simulator):
+    """
+    A state machine simulator that considers the state machine as a simple
+    hierarchical automaton, ie. with no support for history states and
+    orthogonal states.
+    """
     pass
 
 
-# TODO: Simple executor (no support for history nor orthogonal states)
-class SimpleSimulator(Simulator):
+class SCXMLSimulator(Simulator):
+    """
+    A state machine simulator that implements SCXML semantic.
+    """
     pass
-
-
-# TODO: CompleteExecutor (support for history and orthogonal states)
-class CompleteSimulator(Simulator):
-    pass
-
