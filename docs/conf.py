47fb9ff4d29c1724d11c99f0679efff21a289b0f
Alexandre Decan <alexandre.decan@lexpage.net>
2015-12-09 16:28:07 +0100

    Forget to remove apidoc from Sphinx extension.

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,6 @@ import pyss
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.apidoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
