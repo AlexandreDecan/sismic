35ad2071e158c46496baf48c9170bab24762fa53
Alexandre Decan <alexandre.decan@lexpage.net>
2015-12-11 22:14:33 +0100

    0.6.1

--- pyss/__init__.py
+++ pyss/__init__.py
@@ -1,5 +1,5 @@
 __description__ = 'Python Statechart Simulator'
-__version__ = '0.5.5'
+__version__ = '0.6.1'
 __url__ = 'https://github.com/AlexandreDecan/PySS/'
 __author__ = 'Alexandre Decan'
 __email__ = 'alexandre.decan@lexpage.net'
