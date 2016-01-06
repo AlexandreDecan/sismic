93057b26697a34ae79e74f6c80912f7b642fd60c
Alexandre Decan <alexandre.decan@lexpage.net>
2016-01-06 17:56:45 +0100

    Expose testing as a main module

--- sismic/__init__.py
+++ sismic/__init__.py
@@ -1,8 +1,8 @@
 # "Export" most used elements
-from . import evaluator, io, model, interpreter, stories
+from . import evaluator, interpreter, io, model, stories, testing
 
 __description__ = 'Sismic Interactive State Machine Interpreter and Checker'
-__version__ = '0.12.1'
+__version__ = '0.12.2'
 __url__ = 'https://github.com/AlexandreDecan/sismic/'
 __author__ = 'Alexandre Decan'
 __email__ = 'alexandre.decan@lexpage.net'
