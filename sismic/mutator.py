3469e814dd4d282314287f862089ce4eaa13c3c9
Ali Parsai <ali.parsai@email.com>
2016-01-18 18:02:48 +0100

    renamed  RemoveState to StateMissing to match the convention in literature + implemented ArcMissing

--- sismic/mutator.py
+++ sismic/mutator.py
@@ -11,17 +11,21 @@ class Mutator(object):
         assert isinstance(self.statechart, model.StateChart)
         return deepcopy(self.statechart)
 
+    def mutate(self) -> list:
+        pass
 
-class RemoveState(Mutator):
+
+class StateMissing(Mutator):
     def __init__(self, statechart: model.StateChart = None):
-        super(RemoveState, self).__init__(statechart=statechart)
-        self.type = "RemoveState"
+        super(StateMissing, self).__init__(statechart=statechart)
+        self.type = "StateMissing"
 
     def mutate(self) -> list:
         mutants = list()
 
         for state_key in self.statechart.states.keys():
             new_mutant = self.separate_instance()
+            new_mutant.type = self.type
             assert isinstance(new_mutant, model.StateChart)
             new_mutant.removed_state = new_mutant.states.pop(state_key)
             new_mutant.removed_transitions = list()
@@ -34,6 +38,32 @@ class RemoveState(Mutator):
 
             for transition in new_mutant.removed_transitions:
                 new_mutant.transitions.remove(transition)
+
+            try:
+                new_mutant.validate()
+            except AssertionError as e:
+                # print (new_mutant.removed_state, new_mutant.removed_transitions, e)
+                continue
+
+            mutants.append(new_mutant)
+
+        return mutants
+
+
+class ArcMissing(Mutator):
+    def __init__(self, statechart: model.StateChart = None):
+        super(ArcMissing, self).__init__(statechart=statechart)
+        self.type = "ArcMissing"
+
+    def mutate(self) -> list:
+        mutants = list()
+
+        for transition in self.statechart.transitions:
+            new_mutant = self.separate_instance()
+            assert isinstance(new_mutant, model.StateChart)
+            new_mutant.type = self.type
+            new_mutant.removed_transitions = [transition]
+            new_mutant.transitions.remove(transition)
 
             try:
                 new_mutant.validate()
