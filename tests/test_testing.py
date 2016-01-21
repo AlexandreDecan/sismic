635dc26996b2233b101973d332e4ba6340f8438a
Alexandre Decan <alexandre.decan@lexpage.net>
2016-01-21 14:27:18 +0100

    Fix tests

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -11,13 +11,16 @@ class StoryFromTraceTests(unittest.TestCase):
         Story([Pause(2), Event('goto s2'), Pause(3)]).tell(interpreter)
         story = teststory_from_trace(interpreter.trace)
         expected = Story([Event('started'),
+                          Event('step'),
                           Event('entered', state='root'),
                           Event('entered', state='s1'),
                           Pause(2),
+                          Event('step'),
                           Event('consumed', event=Event('goto s2')),
                           Event('exited', state='s1'),
                           Event('processed', source='s1', target='s2', event=Event('goto s2')),
                           Event('entered', state='s2'),
+                          Event('step'),
                           Event('exited', state='s2'),
                           Event('processed', source='s2', target='s3', event=None),
                           Event('entered', state='s3'),
