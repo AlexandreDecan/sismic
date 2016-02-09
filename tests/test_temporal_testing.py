cd478d92ea51fadb262b279a98a2a348a6fb46a4
mgoeminne <mgoeminne@gmail.com>
2016-02-09 17:13:01 +0100

    temporal testing now supports 3 possible status for tested conditions: True, False, and Undefined. Undefined occurs when a condition can still become True or False.

--- tests/test_temporal_testing.py
+++ tests/test_temporal_testing.py
@@ -1,5 +1,5 @@
 import unittest
-from sismic.temporal_testing import Enter, Exit, And, Or, FalseCondition, TrueCondition
+from sismic.temporal_testing import Enter, Exit, And, Or, FalseCondition, TrueCondition, Condition
 from sismic.interpreter import Interpreter
 from sismic.model import Event, Statechart, CompoundState, BasicState, Transition
 from sismic.testing import teststory_from_trace
@@ -18,6 +18,33 @@ class TemporalTests(unittest.TestCase):
         self.sequential_interpreter = Interpreter(self.sequential_statechart)
         self.sequential_interpreter.queue(Event('event'))
 
+    def generic_test(self, condition: Condition, success_expected: bool, failure_expected: bool):
+        statechart = Statechart('test')
+        initial_state = CompoundState('initial_state', initial='Cond')
+        statechart.add_state(initial_state, None)
+
+        success_state = BasicState('success')
+        statechart.add_state(success_state, 'initial_state')
+
+        failure_state = BasicState('failure')
+        statechart.add_state(failure_state, 'initial_state')
+
+        condition.add_to(statechart=statechart, id='Cond', parent_id='initial_state',
+                         success_state_id='success', failure_state_id='failure')
+
+        interpreter = Interpreter(statechart)
+
+        self.assertFalse('success' in interpreter.configuration)
+        self.assertFalse('failure' in interpreter.configuration)
+
+        self.sequential_interpreter.execute()
+        trace = self.sequential_interpreter.trace
+        story = teststory_from_trace(trace)
+        story.tell(interpreter)
+
+        self.assertEqual(success_expected, 'success' in interpreter.configuration)
+        self.assertEqual(failure_expected, 'failure' in interpreter.configuration)
+
     def test_enter(self):
         test_statechart = Statechart('test')
         test_initial_state = CompoundState('initial_state', initial='Enter')
@@ -25,7 +52,7 @@ class TemporalTests(unittest.TestCase):
         out_state = BasicState('out')
         test_statechart.add_state(test_initial_state, None)
         test_statechart.add_state(out_state, 'initial_state')
-        enter_state.add_to(test_statechart, 'Enter', 'initial_state', 'out')
+        enter_state.add_to(test_statechart, 'Enter', 'initial_state', 'out', None)
         test_interpreter = Interpreter(test_statechart)
 
         self.assertFalse('out' in test_interpreter.configuration)
@@ -44,7 +71,7 @@ class TemporalTests(unittest.TestCase):
         out_state = BasicState('out')
         test_statechart.add_state(test_initial_state, None)
         test_statechart.add_state(out_state, 'initial_state')
-        enter_state.add_to(test_statechart, 'Enter', 'initial_state', 'out')
+        enter_state.add_to(test_statechart, 'Enter', 'initial_state', 'out', None)
         test_interpreter = Interpreter(test_statechart)
 
         self.assertFalse('out' in test_interpreter.configuration)
@@ -64,135 +91,41 @@ class TemporalTests(unittest.TestCase):
         statechart = Statechart('test')
         initial_state = CompoundState('initial_state', initial='And')
         and_state = And(Enter('b_state'), Exit('a_state'))
-        out_state = BasicState('out')
+
         statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
+
+        success_state = BasicState('success')
+        statechart.add_state(success_state, 'initial_state')
+
+        failure_state = BasicState('failure')
+        statechart.add_state(failure_state, 'initial_state')
+
+        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state',
+                         success_state_id='success', failure_state_id='failure')
         interpreter = Interpreter(statechart)
 
-        self.assertFalse('out' in interpreter.configuration)
+        self.assertFalse('success' in interpreter.configuration)
+        self.assertFalse('failure' in interpreter.configuration)
 
         self.sequential_interpreter.execute()
         trace = self.sequential_interpreter.trace
         story = teststory_from_trace(trace)
         story.tell(interpreter)
 
-        self.assertTrue('out' in interpreter.configuration)
-
-    def test_and_with_false(self):
-        """
-        Tests if (a is exited AND False) is not satisfied .
-        """
-
-        statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = And(Enter('b_state'), FalseCondition())
-        out_state = BasicState('out')
-        statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
-        interpreter = Interpreter(statechart)
-
-        self.assertFalse('out' in interpreter.configuration)
-
-        self.sequential_interpreter.execute()
-        trace = self.sequential_interpreter.trace
-        story = teststory_from_trace(trace)
-        story.tell(interpreter)
-
-        self.assertFalse('out' in interpreter.configuration)
+        self.assertTrue('success' in interpreter.configuration)
+        self.assertFalse('failure' in interpreter.configuration)
 
     def test_true_and_true(self):
-        """
-        Tests if (a is exited AND False) is not satisfied .
-        """
-
-        statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = And(TrueCondition(), TrueCondition())
-        out_state = BasicState('out')
-        statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
-        interpreter = Interpreter(statechart)
-
-        self.assertFalse('out' in interpreter.configuration)
-
-        self.sequential_interpreter.execute()
-        trace = self.sequential_interpreter.trace
-        story = teststory_from_trace(trace)
-        story.tell(interpreter)
-
-        self.assertTrue('out' in interpreter.configuration)
+        self.generic_test(And(TrueCondition(), TrueCondition()), True, False)
 
     def test_true_and_false(self):
-        """
-        Tests if (a is exited AND False) is not satisfied .
-        """
-
-        statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = And(TrueCondition(), FalseCondition())
-        out_state = BasicState('out')
-        statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
-        interpreter = Interpreter(statechart)
-
-        self.assertFalse('out' in interpreter.configuration)
-
-        self.sequential_interpreter.execute()
-        trace = self.sequential_interpreter.trace
-        story = teststory_from_trace(trace)
-        story.tell(interpreter)
-
-        self.assertFalse('out' in interpreter.configuration)
+        self.generic_test(And(TrueCondition(), FalseCondition()), False, True)
 
     def test_false_and_true(self):
-        """
-        Tests if (a is exited AND False) is not satisfied .
-        """
-
-        statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = And(FalseCondition(), TrueCondition())
-        out_state = BasicState('out')
-        statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
-        interpreter = Interpreter(statechart)
-
-        self.assertFalse('out' in interpreter.configuration)
-
-        self.sequential_interpreter.execute()
-        trace = self.sequential_interpreter.trace
-        story = teststory_from_trace(trace)
-        story.tell(interpreter)
-
-        self.assertFalse('out' in interpreter.configuration)
+        self.generic_test(And(FalseCondition(), TrueCondition()), False, True)
 
     def test_false_and_false(self):
-        """
-        Tests if (a is exited AND False) is not satisfied .
-        """
-
-        statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = And(FalseCondition(), FalseCondition())
-        out_state = BasicState('out')
-        statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
-        interpreter = Interpreter(statechart)
-
-        self.assertFalse('out' in interpreter.configuration)
-
-        self.sequential_interpreter.execute()
-        trace = self.sequential_interpreter.trace
-        story = teststory_from_trace(trace)
-        story.tell(interpreter)
-
-        self.assertFalse('out' in interpreter.configuration)
+        self.generic_test(And(FalseCondition(), FalseCondition()), False, True)
 
     def test_or(self):
         """
@@ -200,98 +133,44 @@ class TemporalTests(unittest.TestCase):
         """
 
         statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = Or(Enter('b_state'), Exit('a_state'))
-        out_state = BasicState('out')
+        initial_state = CompoundState('initial_state', initial='Or')
         statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
+
+        or_state = Or(Enter('b_state'), Exit('a_state'))
+
+        success_state = BasicState('success')
+        statechart.add_state(success_state, 'initial_state')
+
+        failure_state = BasicState('failure')
+        statechart.add_state(failure_state, 'initial_state')
+
+        or_state.add_to(statechart=statechart, id='Or', parent_id='initial_state',
+                        success_state_id='success', failure_state_id='failure')
+
         interpreter = Interpreter(statechart)
 
-        self.assertFalse('out' in interpreter.configuration)
+        self.assertFalse('success' in interpreter.configuration)
+        self.assertFalse('failure' in interpreter.configuration)
 
         self.sequential_interpreter.execute()
         trace = self.sequential_interpreter.trace
         story = teststory_from_trace(trace)
         story.tell(interpreter)
 
-        self.assertTrue('out' in interpreter.configuration)
+        self.assertTrue('success' in interpreter.configuration)
+        self.assertFalse('failure' in interpreter.configuration)
 
     def test_true_or_true(self):
-        statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = Or(TrueCondition(), TrueCondition())
-        out_state = BasicState('out')
-        statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
-        interpreter = Interpreter(statechart)
-
-        self.assertFalse('out' in interpreter.configuration)
-
-        self.sequential_interpreter.execute()
-        trace = self.sequential_interpreter.trace
-        story = teststory_from_trace(trace)
-        story.tell(interpreter)
-
-        self.assertTrue('out' in interpreter.configuration)
+        self.generic_test(Or(TrueCondition(), TrueCondition()), True, False)
 
     def test_true_or_false(self):
-        statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = Or(TrueCondition(), FalseCondition())
-        out_state = BasicState('out')
-        statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
-        interpreter = Interpreter(statechart)
-
-        self.assertFalse('out' in interpreter.configuration)
-
-        self.sequential_interpreter.execute()
-        trace = self.sequential_interpreter.trace
-        story = teststory_from_trace(trace)
-        story.tell(interpreter)
-
-        self.assertTrue('out' in interpreter.configuration)
+        self.generic_test(Or(TrueCondition(), FalseCondition()), True, False)
 
     def test_false_or_true(self):
-        statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = Or(FalseCondition(), TrueCondition())
-        out_state = BasicState('out')
-        statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
-        interpreter = Interpreter(statechart)
-
-        self.assertFalse('out' in interpreter.configuration)
-
-        self.sequential_interpreter.execute()
-        trace = self.sequential_interpreter.trace
-        story = teststory_from_trace(trace)
-        story.tell(interpreter)
-
-        self.assertTrue('out' in interpreter.configuration)
+        self.generic_test(Or(FalseCondition(), TrueCondition()), True, False)
 
     def test_false_or_false(self):
-        statechart = Statechart('test')
-        initial_state = CompoundState('initial_state', initial='And')
-        and_state = Or(FalseCondition(), FalseCondition())
-        out_state = BasicState('out')
-        statechart.add_state(initial_state, None)
-        statechart.add_state(out_state, 'initial_state')
-        and_state.add_to(statechart=statechart, id='And', parent_id='initial_state', success_state_id='out')
-        interpreter = Interpreter(statechart)
-
-        self.assertFalse('out' in interpreter.configuration)
-
-        self.sequential_interpreter.execute()
-        trace = self.sequential_interpreter.trace
-        story = teststory_from_trace(trace)
-        story.tell(interpreter)
-
-        self.assertFalse('out' in interpreter.configuration)
+        self.generic_test(Or(FalseCondition(), FalseCondition()), False, True)
 
 
 
