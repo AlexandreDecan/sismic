a5c1c61eb7346341635fa39995a1de27727955c2
Alexandre Decan <alexandre.decan@lexpage.net>
2016-02-18 10:59:05 +0100

    Basic support for JS

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -68,6 +68,10 @@ class PythonEvaluatorTests(unittest.TestCase):
         self.evaluator._execute_code('send("hello")')
         self.interpreter.queue.assert_called_with(InternalEvent('hello'))
 
+    def test_send_with_parameters(self):
+        self.evaluator._execute_code('send("hello", floor=4)')
+        self.interpreter.queue.assert_called_with(InternalEvent('hello', floor=4))
+
     def test_add_variable_in_context(self):
         self.evaluator._execute_code('a = 1\nassert a == 1')
         self.assertTrue(self.evaluator._evaluate_code('a == 1'))
@@ -75,3 +79,58 @@ class PythonEvaluatorTests(unittest.TestCase):
     @unittest.skip('http://stackoverflow.com/questions/32894942/listcomp-unable-to-access-locals-defined-in-code-called-by-exec-if-nested-in-fun')
     def test_access_outer_scope(self):
         self.evaluator._execute_code('a = 1\nd = [x for x in range(10) if x!=a]')
+
+
+class JavascriptEvaluatorTests(unittest.TestCase):
+    def setUp(self):
+        context = {
+            'x': 1,
+            'y': 2,
+            'z': 3
+        }
+
+        interpreter = MagicMock(name='Interpreter')
+        interpreter.time = MagicMock(return_value=0)
+        interpreter.queue = MagicMock(return_value=None)
+        interpreter.configuration = MagicMock(return_value=[])
+
+        self.evaluator = code.JavascriptEvaluator(interpreter=interpreter, initial_context=context)
+        self.interpreter = interpreter
+
+    def test_condition(self):
+        self.assertTrue(self.evaluator._evaluate_code('true'))
+        self.assertFalse(self.evaluator._evaluate_code('false'))
+        self.assertTrue(self.evaluator._evaluate_code('1 == 1'))
+        self.assertTrue(self.evaluator._evaluate_code('x == 1'))
+        with self.assertRaises(Exception):
+            self.evaluator._evaluate_code('a')
+
+    def test_condition_on_event(self):
+        self.assertTrue(self.evaluator._evaluate_code('event.data[\'a\'] == 1', {'event': Event('test', a=1)}))
+        self.assertTrue(self.evaluator._evaluate_code('event.name == \'test\'', {'event': Event('test')}))
+
+    def test_execution(self):
+        self.evaluator._execute_code('a = 1')
+        self.assertEqual(self.evaluator.context['a'], 1)
+        self.evaluator._execute_code('x = 2')
+        self.assertEqual(self.evaluator.context['x'], 2)
+
+    def test_invalid_condition(self):
+        with self.assertRaises(exceptions.CodeEvaluationError):
+            self.evaluator._evaluate_code('unknown()')
+
+    def test_invalid_action(self):
+        with self.assertRaises(exceptions.CodeEvaluationError):
+            self.evaluator._execute_code('x = unknown()')
+
+    def test_send(self):
+        self.evaluator._execute_code('send("hello")')
+        self.interpreter.queue.assert_called_with(InternalEvent('hello'))
+
+    def test_send_with_parameters(self):
+        self.evaluator._execute_code('send("hello", {floor:4})')
+        self.interpreter.queue.assert_called_with(InternalEvent('hello', floor=4))
+
+    def test_add_variable_in_context(self):
+        self.evaluator._execute_code('a = 1')
+        self.assertTrue(self.evaluator._evaluate_code('a == 1'))
