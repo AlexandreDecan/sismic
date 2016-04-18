099b7f730e5e959168d617b369019599743e6e59
Alexandre Decan <alexandre.decan@lexpage.net>
2016-04-18 15:48:10 +0200

    Fix typing dependency

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyyaml>=3.11', 'pykwalify>=1.5.1', 'behave>=1.2.5', 'typing>=3.5.0.1'],
+    install_requires=['pyyaml>=3.11', 'pykwalify>=1.5.1', 'behave>=1.2.5', 'typing==3.5.0.1'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
