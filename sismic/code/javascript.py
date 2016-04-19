de59fa15dbd5cd20f9991b96ca0e57c2e348d2d5
Alexandre Decan <alexandre.decan@lexpage.net>
2016-04-19 11:35:19 +0200

    Merge branch 'devel' into js-code
    # Conflicts:
    #	setup.py
    #	tests/test_evaluator.py

--- sismic/code/javascript.py
+++ sismic/code/javascript.py
@@ -1,30 +1,33 @@
-from functools import partial
+from typing import cast, Iterator, Mapping
+from typing import Dict, Iterator, cast, Any, Mapping, MutableMapping
 
 import js2py
-from sismic.code import Evaluator
+from .evaluator import Evaluator
+from sismic.model import Event, InternalEvent, Transition, StateMixin, Statechart
 from sismic.exceptions import CodeEvaluationError
-from sismic.model import InternalEvent
 
 __all__ = ['JavascriptEvaluator']
 
 
 class JavascriptEvaluator(Evaluator):
-    def __init__(self, interpreter=None, initial_context: dict=None):
-        super().__init__(interpreter, initial_context)
-        self._js = js2py.EvalJs(initial_context)
+    def __init__(self, interpreter=None, *, initial_context: Mapping=None) -> None:
+        super().__init__()
+
+        self._interpreter = interpreter
+        self._js = js2py.EvalJs(initial_context if initial_context else {})
 
     @property
     def context(self):
         return self._js.__dict__['_var']
 
     def __send(self, name, parameters=None):
-        if not parameters is None:
+        if parameters is not None:
             parameters = parameters.to_dict()
         else:
             parameters = {}
         self._interpreter.queue(InternalEvent(name, **parameters))
 
-    def _execute_code(self, code: str, additional_context: dict=None):
+    def _execute_code(self, code: str, *, additional_context: Mapping=None) -> None:
         exposed_context = {
             'send': self.__send,
         }
@@ -34,11 +37,11 @@ class JavascriptEvaluator(Evaluator):
             setattr(self._js, k, v)
 
         try:
-            return self._js.execute(code)
+            self._js.execute(code)
         except Exception as e:
             raise CodeEvaluationError('The above exception occurred while executing:\n{}'.format(code)) from e
 
-    def _evaluate_code(self, code: str, additional_context: dict=None):
+    def _evaluate_code(self, code: str, *, additional_context: Mapping=None) -> bool:
         exposed_context = {}
         exposed_context.update(additional_context if additional_context else {})
 
