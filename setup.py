684c231afde7cb2a188fd546797be781b83ab238
Alexandre Decan <alexandre.decan@lexpage.net>
2016-04-29 13:36:37 +0200

    Add required dependencies

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyyaml>=3.11', 'pykwalify>=1.5.1', 'behave>=1.2.5', 'typing>=3.5.1'],
+    install_requires=['pyyaml>=3.11', 'pykwalify>=1.5.1', 'behave>=1.2.5', 'typing>=3.5.1', 'redbaron>=0.6.1', 'mypy-lang>=0.3.1'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
