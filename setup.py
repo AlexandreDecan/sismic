937003393a73f87a79c04dc17a75c1d2a8c9952a
Alexandre Decan <alexandre.decan@lexpage.net>
2017-05-02 09:21:41 +0200

    Prevent pip to install package if python < 3.4

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
 
     ],
 
@@ -68,6 +69,8 @@ setup(
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
     #py_modules=['sismic'],
+
+    python_requires='>=3.4',
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
