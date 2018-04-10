ea35b8a851fec0b4a36e9e5a0c7a01a9313aac47
Alexandre Decan <alexandre.decan@lexpage.net>
2018-04-10 13:41:50 +0200

    setup.py cleaned

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@
 from codecs import open
 from os import path
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 import sismic
-
-# python setup.py register
-# python setup.py sdist bdist_wheel upload
 
 here = path.abspath(path.dirname(__file__))
 
@@ -17,25 +14,13 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='sismic',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version=sismic.__version__,
-
     description=sismic.__description__,
     long_description=long_description,
-
-    # The project's main homepage.
     url=sismic.__url__,
-
-    # Author details
     author=sismic.__author__,
     author_email=sismic.__email__,
-
-    # Choose your license
     license=sismic.__licence__,
-
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -58,42 +43,25 @@ setup(
         'Programming Language :: Python :: 3.6',
 
     ],
-
-    # What does your project relate to?
     keywords='statechart state machine interpreter model uml scxml harel',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #py_modules=['sismic'],
-
+    packages=['sismic'],
     python_requires='>=3.4',
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['ruamel.yaml>=0.12.10', 'schema>=0.6.2', 'behave>=1.2.6', 'typing>=3.5.1'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        #'dev': ['check-manifest'],
-    },
-
-    include_package_data=True,
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
+    install_requires=[
+        'ruamel.yaml>=0.12.10',
+        'schema>=0.6.2',
+        'behave>=1.2.6',
+        'typing>=3.5.1'
+    ],
     entry_points={
         'console_scripts': [
             'sismic-bdd=sismic.bdd.__main__:cli',
         ],
     },
+    # package_data and data_files can be used to populate MANIFEST.in
+    # See https://docs.python.org/3.6/distutils/settupscript.html#installing-package-data
+    package_data=[],
+    # See https://docs.python.org/3.6/distutils/settupscript.html#installing-additional-files
+    data_files=[],
+    # Files listed in MANIFEST.in (generated or manually completed) will be included upon installation
+    include_package_data=True,
 )
