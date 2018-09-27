2d49a46d10d797c550037eaf70e7d72d24743f4c
Timothy Rule <tim.rule.xing@gmail.com>
2018-09-27 14:21:43 +0200

    Refactor code/python.py into two classes:
      code/enhanced.py - implements enhanced evaluator functionality.
      code/python.py - implements the Python code evaluation.
    
    It's now possible to create a Code Evaluator which inherits from
    either the Evaluator or EnhancedEvaluator Class, depending on the
    desired behaviour required for the Code Evaluator.
    
    Eventually the EnhancedEvaluator Class may be converted to a Mixin
    Class so that additional behaviours can be added to an Evaluator
    without needing to refactor exising API interfaces.

--- sismic/code/__init__.py
+++ sismic/code/__init__.py
@@ -1,5 +1,6 @@
 from .evaluator import Evaluator
 from .dummy import DummyEvaluator
+from .enhanced import EnhancedEvaluator
 from .python import PythonEvaluator
 
-__all__ = ['Evaluator', 'DummyEvaluator', 'PythonEvaluator']
+__all__ = ['Evaluator', 'DummyEvaluator', 'EnhancedEvaluator', 'PythonEvaluator']
