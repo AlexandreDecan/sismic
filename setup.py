ee25805600d511838b299a5db3613584a1957c32
Alexandre Decan <alexandre.decan@lexpage.net>
2025-10-10 10:12:36 +0200

    Python 3.14

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
     ],
     keywords='statechart state machine interpreter model uml scxml harel',
 
